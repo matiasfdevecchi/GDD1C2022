aaecab85407daad3892082143fe6c72c4bb03a02
Matias F. Devecchi Di Bella <matiasfdevecchi@gmail.com>
2022-06-24 01:14:25 -0300

    Finalizado carga de hechos

--- script_creación_BI.sql
+++ script_creación_BI.sql
@@ -4,6 +4,12 @@ GO
 
 --Sentencia para que sea reejecutable el script
 
+IF OBJECT_ID('Data_Center_Group.BI_FACT_Telemetria', 'U') IS NOT NULL
+    DROP TABLE [Data_Center_Group].BI_FACT_Telemetria;
+IF OBJECT_ID('Data_Center_Group.BI_FACT_Parada', 'U') IS NOT NULL
+    DROP TABLE [Data_Center_Group].BI_FACT_Parada;
+IF OBJECT_ID('Data_Center_Group.BI_FACT_IncidenteAuto', 'U') IS NOT NULL
+    DROP TABLE [Data_Center_Group].BI_FACT_IncidenteAuto;
 IF OBJECT_ID('Data_Center_Group.BI_DIM_Tiempo', 'U') IS NOT NULL
     DROP TABLE [Data_Center_Group].BI_DIM_Tiempo;
 IF OBJECT_ID('Data_Center_Group.BI_DIM_Auto', 'U') IS NOT NULL
@@ -18,8 +24,8 @@ IF OBJECT_ID('Data_Center_Group.BI_DIM_Sector', 'U') IS NOT NULL
     DROP TABLE [Data_Center_Group].BI_DIM_Sector;
 IF OBJECT_ID('Data_Center_Group.BI_DIM_Neumatico', 'U') IS NOT NULL
     DROP TABLE [Data_Center_Group].BI_DIM_Neumatico;
-IF OBJECT_ID('Data_Center_Group.BI_DIM_Incidente', 'U') IS NOT NULL
-    DROP TABLE [Data_Center_Group].BI_DIM_Incidente;
+IF OBJECT_ID('Data_Center_Group.BI_DIM_IncidenteAuto', 'U') IS NOT NULL
+    DROP TABLE [Data_Center_Group].BI_DIM_IncidenteAuto;
 
 /*IF OBJECT_ID('Data_Center_Group.BI_view_desgaste_promedio_cada_auto_x_vuelta_x_circuito', 'V') IS NOT NULL
     DROP VIEW Data_Center_Group.BI_view_desgaste_promedio_cada_auto_x_vuelta_x_circuito;
@@ -75,78 +81,76 @@ CREATE TABLE [Data_Center_Group].BI_DIM_Piloto (
 );
 
 CREATE TABLE [Data_Center_Group].BI_DIM_Sector (
-	id INT NOT NULL IDENTITY PRIMARY KEY,
+	codigo INT NOT NULL PRIMARY KEY,
 	tipo NVARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE [Data_Center_Group].BI_DIM_Neumatico (
-	id INT NOT NULL IDENTITY PRIMARY KEY,
+	numero_serie NVARCHAR(255) NOT NULL PRIMARY KEY,
 	tipo NVARCHAR(255) NOT NULL,
 );
 
-CREATE TABLE [Data_Center_Group].BI_DIM_Incidente (
-	id INT NOT NULL IDENTITY PRIMARY KEY,
-	tipo NVARCHAR(255) NOT NULL,
+CREATE TABLE [Data_Center_Group].BI_DIM_IncidenteAuto (
+	tipo NVARCHAR(255) NOT NULL PRIMARY KEY,
 );
 
 -- Tabla de hechos
 CREATE TABLE [Data_Center_Group].BI_FACT_Telemetria(
-    id INT NOT NULL PRIMARY KEY, 
+    id INT NOT NULL IDENTITY PRIMARY KEY, 
 	tiempo_id INTEGER FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Tiempo(id),
 	auto_id INTEGER FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Auto(id),
 	escuderia_nombre NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Escuderia(nombre),
 	piloto_codigo INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Piloto(codigo),
-	sector_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Sector(id),
+	sector_codigo INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Sector(codigo),
 	circuito_codigo INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Circuito(codigo),
-	neumatico_del_izq_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_del_der_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_tra_izq_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_tra_der_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
+	neumatico_del_izq_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_del_der_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_tra_izq_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_tra_der_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
 	numero_vuelta DECIMAL(18,0) NOT NULL,
 	tiempo_vuelta DECIMAL(18,10) NOT NULL,
 	caja_desgaste DECIMAL(18,2) NOT NULL,
-	motor_desgaste DECIMAL(18,6) NOT NULL,
-	neumatico_del_izq_desgaste DECIMAL(18,6) NOT NULL,
-	neumatico_del_der_desgaste DECIMAL(18,6) NOT NULL,
-	neumatico_tra_izq_desgaste DECIMAL(18,6) NOT NULL,
-	neumatico_tra_der_desgaste DECIMAL(18,6) NOT NULL,
-	freno_del_izq_desgaste DECIMAL(18,2) NOT NULL,
-	freno_del_der_desgaste DECIMAL(18,2) NOT NULL,
-	freno_tra_izq_desgaste DECIMAL(18,2) NOT NULL,
-	freno_tra_der_desgaste DECIMAL(18,2) NOT NULL,
+	motor_potencia DECIMAL(18,6) NOT NULL,
+	neumatico_del_izq_profundidad DECIMAL(18,6) NOT NULL,
+	neumatico_del_der_profundidad DECIMAL(18,6) NOT NULL,
+	neumatico_tra_izq_profundidad DECIMAL(18,6) NOT NULL,
+	neumatico_tra_der_profundidad DECIMAL(18,6) NOT NULL,
+	freno_del_izq_grosor_pastilla DECIMAL(18,2) NOT NULL,
+	freno_del_der_grosor_pastilla DECIMAL(18,2) NOT NULL,
+	freno_tra_izq_grosor_pastilla DECIMAL(18,2) NOT NULL,
+	freno_tra_der_grosor_pastilla DECIMAL(18,2) NOT NULL,
 	velocidad DECIMAL(18,2) NOT NULL
 );
 
 CREATE TABLE [Data_Center_Group].BI_FACT_Parada(
-    id INT NOT NULL PRIMARY KEY, 
+    id INT NOT NULL IDENTITY PRIMARY KEY, 
 	tiempo_id INTEGER FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Tiempo(id),
 	auto_id INTEGER FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Auto(id),
 	escuderia_nombre NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Escuderia(nombre),
 	piloto_codigo INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Piloto(codigo),
 	circuito_codigo INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Circuito(codigo),
-	neumatico_del_izq_viejo_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_del_izq_nuevo_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_del_der_viejo_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_del_der_nuevo_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_tra_izq_viejo_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_tra_izq_nuevo_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_tra_der_viejo_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
-	neumatico_tra_der_nuevo_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(id),
+	neumatico_del_izq_viejo_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_del_izq_nuevo_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_del_der_viejo_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_del_der_nuevo_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_tra_izq_viejo_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_tra_izq_nuevo_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_tra_der_viejo_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
+	neumatico_tra_der_nuevo_numero_serie NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Neumatico(numero_serie),
 	numero_vuelta DECIMAL(18,0) NOT NULL,
 	tiempo DECIMAL(18,2) NOT NULL
 );
 
-CREATE TABLE [Data_Center_Group].BI_FACT_Incidente(
-    id INT NOT NULL PRIMARY KEY, 
+CREATE TABLE [Data_Center_Group].BI_FACT_IncidenteAuto(
+    id INT NOT NULL IDENTITY PRIMARY KEY, 
 	tiempo_id INTEGER FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Tiempo(id),
 	auto_id INTEGER FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Auto(id),
 	escuderia_nombre NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Escuderia(nombre),
 	piloto_codigo INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Piloto(codigo),
 	circuito_codigo INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Circuito(codigo),
-	sector_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Sector(id),
-	incidente_id INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Incidente(id),
-	numero_vuelta DECIMAL(18,0) NOT NULL,
-	tiempo DECIMAL(18,2) NOT NULL
+	sector_codigo INT FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_Sector(codigo),
+	incidente_auto_tipo NVARCHAR(255) FOREIGN KEY REFERENCES [Data_Center_Group].BI_DIM_IncidenteAuto(tipo),
+	numero_vuelta DECIMAL(18,0) NOT NULL
 );
 
 --Carga de datos de tiempo (falta)
@@ -195,7 +199,7 @@ CREATE PROCEDURE [Data_Center_Group].cargarDIMPilotos
 AS
 BEGIN
 	INSERT INTO [Data_Center_Group].BI_DIM_Piloto
-	SELECT DISTINCT nombre, apellido, nacionalidad, fecha_nacimiento
+	SELECT DISTINCT codigo, nombre, apellido, nacionalidad, fecha_nacimiento
 	FROM  [Data_Center_Group].Piloto
 END;
 
@@ -206,7 +210,7 @@ CREATE PROCEDURE [Data_Center_Group].cargarDIMSectores
 AS
 BEGIN
 	INSERT INTO [Data_Center_Group].BI_DIM_Sector
-	SELECT DISTINCT tipo
+	SELECT codigo, tipo
 	FROM [Data_Center_Group].Sector
 END;
 
@@ -216,18 +220,174 @@ CREATE PROCEDURE [Data_Center_Group].cargarDIMNeumaticos
 AS
 BEGIN
 	INSERT INTO [Data_Center_Group].BI_DIM_Neumatico
-	SELECT DISTINCT tipo
+	SELECT numero_serie, tipo
 	FROM [Data_Center_Group].Neumatico
 END;
 
 --Carga de datos de incidente
 GO
-CREATE PROCEDURE [Data_Center_Group].cargarDIMIncidentes
+CREATE PROCEDURE [Data_Center_Group].cargarDIMIncidenteAutos
 AS
 BEGIN
-	INSERT INTO [Data_Center_Group].BI_DIM_Incidente
-	SELECT DISTINCT bandera
-	FROM [Data_Center_Group].Incidente
+	INSERT INTO [Data_Center_Group].BI_DIM_IncidenteAuto
+	SELECT DISTINCT tipo
+	FROM [Data_Center_Group].IncidenteAuto
+END;
+
+--Carga de datos de telemetria
+GO
+CREATE FUNCTION [Data_Center_Group].retornarTiempo(@fecha DATE)
+RETURNS INT
+BEGIN
+	RETURN (
+    SELECT id FROM [Data_Center_Group].BI_DIM_Tiempo t
+    WHERE t.anio = YEAR(@fecha) AND t.cuatrimestre = MONTH(@fecha)
+    )
+END;
+
+GO
+CREATE FUNCTION [Data_Center_Group].retornarAuto(@numero INT, @modelo NVARCHAR(255))
+RETURNS INT
+BEGIN
+	RETURN (
+    SELECT id FROM [Data_Center_Group].BI_DIM_Auto a
+    WHERE a.modelo = @modelo AND a.numero = @numero
+    )
+END;
+
+GO
+CREATE FUNCTION [Data_Center_Group].retornarNeumaticoDeTelemetria(@telemetria_codigo DECIMAL(18,0), @posicion NVARCHAR(255))
+RETURNS NVARCHAR(255)
+BEGIN
+	RETURN (
+    SELECT tn.neumatico_numero_serie FROM [Data_Center_Group].TelemetriaNeumatico tn
+    WHERE tn.telemetria_codigo = @telemetria_codigo AND tn.posicion = @posicion
+    )
+END;
+
+GO
+CREATE FUNCTION [Data_Center_Group].retornarProfundidadDeTelemetriaNeumatico(@telemetria_codigo DECIMAL(18,0), @posicion NVARCHAR(255))
+RETURNS DECIMAL(18,6)
+BEGIN
+	RETURN (
+    SELECT tn.profundidad FROM [Data_Center_Group].TelemetriaNeumatico tn
+    WHERE tn.telemetria_codigo = @telemetria_codigo AND tn.posicion = @posicion
+    )
+END;
+
+GO
+CREATE FUNCTION [Data_Center_Group].retornarGrosorPastillaDeTelemetriaFreno(@telemetria_codigo DECIMAL(18,0), @posicion NVARCHAR(255))
+RETURNS DECIMAL(18,6)
+BEGIN
+	RETURN (
+    SELECT tf.grosor_pastilla FROM [Data_Center_Group].TelemetriaFreno tf
+    WHERE tf.telemetria_codigo = @telemetria_codigo AND tf.posicion = @posicion
+    )
+END;
+
+GO
+CREATE PROCEDURE [Data_Center_Group].cargarFACTTelemetrias
+AS
+BEGIN
+	INSERT INTO [Data_Center_Group].BI_FACT_Telemetria
+	SELECT 
+		[Data_Center_Group].retornarTiempo(c.fecha), 
+		[Data_Center_Group].retornarAuto(a.numero, a.modelo), 
+		a.escuderia_nombre, 
+		a.piloto_codigo, 
+		t.sector_codigo, 
+		c.circuito_codigo, 
+		[Data_Center_Group].retornarNeumaticoDeTelemetria(t.codigo, 'Delantero Izquierdo'), 
+		[Data_Center_Group].retornarNeumaticoDeTelemetria(t.codigo, 'Delantero Derecho'), 
+		[Data_Center_Group].retornarNeumaticoDeTelemetria(t.codigo, 'Trasero Izquierdo'), 
+		[Data_Center_Group].retornarNeumaticoDeTelemetria(t.codigo, 'Trasero Derecho'),
+		t.vuelta_numero,
+		t.vuelta_tiempo,
+		tc.desgaste,
+		tm.potencia,
+		[Data_Center_Group].retornarProfundidadDeTelemetriaNeumatico(t.codigo, 'Delantero Izquierdo'),
+		[Data_Center_Group].retornarProfundidadDeTelemetriaNeumatico(t.codigo, 'Delantero Derecho'), 
+		[Data_Center_Group].retornarProfundidadDeTelemetriaNeumatico(t.codigo, 'Trasero Izquierdo'), 
+		[Data_Center_Group].retornarProfundidadDeTelemetriaNeumatico(t.codigo, 'Trasero Derecho'),
+		[Data_Center_Group].retornarGrosorPastillaDeTelemetriaFreno(t.codigo, 'Delantero Izquierdo'),
+		[Data_Center_Group].retornarGrosorPastillaDeTelemetriaFreno(t.codigo, 'Delantero Derecho'), 
+		[Data_Center_Group].retornarGrosorPastillaDeTelemetriaFreno(t.codigo, 'Trasero Izquierdo'), 
+		[Data_Center_Group].retornarGrosorPastillaDeTelemetriaFreno(t.codigo, 'Trasero Derecho'),
+		velocidad
+	FROM [Data_Center_Group].Telemetria t
+	JOIN [Data_Center_Group].TelemetriaAuto ta ON ta.telemetria_codigo = t.codigo
+	JOIN [Data_Center_Group].TelemetriaCaja tc ON tc.telemetria_codigo = t.codigo
+	JOIN [Data_Center_Group].TelemetriaMotor tm ON tm.telemetria_codigo = t.codigo
+	JOIN [Data_Center_Group].Carrera c ON c.codigo = t.carrera_codigo
+	JOIN [Data_Center_Group].Auto a ON a.numero = ta.auto_numero AND a.escuderia_nombre = ta.auto_escuderia_nombre
+END;
+
+
+
+GO
+CREATE FUNCTION [Data_Center_Group].retornarNeumaticoViejoDeParada(@parada_codigo DECIMAL(18,0), @posicion NVARCHAR(255))
+RETURNS NVARCHAR(255)
+BEGIN
+	RETURN (
+    SELECT p.neumatico_viejo_numero_serie FROM [Data_Center_Group].ParadaCambioNeumatico p
+    WHERE p.parada_codigo = @parada_codigo AND p.posicion = @posicion
+    )
+END;
+
+GO
+CREATE FUNCTION [Data_Center_Group].retornarNeumaticoNuevoDeParada(@parada_codigo DECIMAL(18,0), @posicion NVARCHAR(255))
+RETURNS NVARCHAR(255)
+BEGIN
+	RETURN (
+    SELECT p.neumatico_viejo_numero_serie FROM [Data_Center_Group].ParadaCambioNeumatico p
+    WHERE p.parada_codigo = @parada_codigo AND p.posicion = @posicion
+    )
+END;
+
+GO
+CREATE PROCEDURE [Data_Center_Group].cargarFACTParadas
+AS
+BEGIN
+	INSERT INTO [Data_Center_Group].BI_FACT_Parada
+	SELECT 
+		[Data_Center_Group].retornarTiempo(c.fecha), 
+		[Data_Center_Group].retornarAuto(a.numero, a.modelo), 
+		a.escuderia_nombre, 
+		a.piloto_codigo, 
+		c.circuito_codigo, 
+		[Data_Center_Group].retornarNeumaticoViejoDeParada(p.codigo, 'Delantero Izquierdo'), 
+		[Data_Center_Group].retornarNeumaticoNuevoDeParada(p.codigo, 'Delantero Izquierdo'), 
+		[Data_Center_Group].retornarNeumaticoViejoDeParada(p.codigo, 'Delantero Derecho'), 
+		[Data_Center_Group].retornarNeumaticoNuevoDeParada(p.codigo, 'Delantero Derecho'), 
+		[Data_Center_Group].retornarNeumaticoViejoDeParada(p.codigo, 'Trasero Izquierdo'), 
+		[Data_Center_Group].retornarNeumaticoNuevoDeParada(p.codigo, 'Trasero Izquierdo'), 
+		[Data_Center_Group].retornarNeumaticoViejoDeParada(p.codigo, 'Trasero Derecho'),
+		[Data_Center_Group].retornarNeumaticoNuevoDeParada(p.codigo, 'Trasero Derecho'),
+		p.numero_vuelta,
+		p.tiempo
+	FROM [Data_Center_Group].Parada p
+	JOIN [Data_Center_Group].Carrera c ON p.carrera_codigo = c.codigo
+	JOIN [Data_Center_Group].Auto a ON a.numero = p.auto_numero AND a.escuderia_nombre = p.auto_escuderia_nombre
+END;
+
+GO
+CREATE PROCEDURE [Data_Center_Group].cargarFACTIncidenteAutos
+AS
+BEGIN
+	INSERT INTO [Data_Center_Group].BI_FACT_IncidenteAuto
+	SELECT 
+		[Data_Center_Group].retornarTiempo(c.fecha), 
+		[Data_Center_Group].retornarAuto(a.numero, a.modelo), 
+		a.escuderia_nombre, 
+		a.piloto_codigo, 
+		c.circuito_codigo, 
+		i.sector_codigo,
+		ia.tipo,
+		ia.numero_vuelta
+	FROM [Data_Center_Group].Incidente i
+	JOIN [Data_Center_Group].IncidenteAuto ia ON i.codigo = ia.incidente_codigo
+	JOIN [Data_Center_Group].Carrera c ON i.carrera_codigo = c.codigo
+	JOIN [Data_Center_Group].Auto a ON a.numero = ia.auto_numero AND a.escuderia_nombre = ia.auto_escuderia_nombre
 END;
 
 GO
@@ -238,7 +398,10 @@ EXEC [Data_Center_Group].cargarDIMCircuitos;
 EXEC [Data_Center_Group].cargarDIMPilotos;
 EXEC [Data_Center_Group].cargarDIMSectores;
 EXEC [Data_Center_Group].cargarDIMNeumaticos;
-EXEC [Data_Center_Group].cargarDIMIncidentes;
+EXEC [Data_Center_Group].cargarDIMIncidenteAutos;
+EXEC  [Data_Center_Group].cargarFACTTelemetrias;
+EXEC  [Data_Center_Group].cargarFACTParadas;
+EXEC  [Data_Center_Group].cargarFACTIncidenteAutos;
 
 DROP PROCEDURE [Data_Center_Group].cargarDIMTiempos;
 DROP PROCEDURE [Data_Center_Group].cargarDIMAutos;
@@ -247,8 +410,18 @@ DROP PROCEDURE [Data_Center_Group].cargarDIMCircuitos;
 DROP PROCEDURE [Data_Center_Group].cargarDIMPilotos;
 DROP PROCEDURE [Data_Center_Group].cargarDIMSectores;
 DROP PROCEDURE [Data_Center_Group].cargarDIMNeumaticos;
-DROP PROCEDURE [Data_Center_Group].cargarDIMIncidentes;
+DROP PROCEDURE [Data_Center_Group].cargarDIMIncidenteAutos;
+DROP PROCEDURE [Data_Center_Group].cargarFACTTelemetrias;
+DROP PROCEDURE [Data_Center_Group].cargarFACTParadas;
+DROP PROCEDURE [Data_Center_Group].cargarFACTIncidenteAutos;
 
+DROP FUNCTION [Data_Center_Group].retornarTiempo;
+DROP FUNCTION [Data_Center_Group].retornarAuto;
+DROP FUNCTION [Data_Center_Group].retornarNeumaticoDeTelemetria;
+DROP FUNCTION [Data_Center_Group].retornarProfundidadDeTelemetriaNeumatico;
+DROP FUNCTION [Data_Center_Group].retornarGrosorPastillaDeTelemetriaFreno;
+DROP FUNCTION [Data_Center_Group].retornarNeumaticoViejoDeParada;
+DROP FUNCTION [Data_Center_Group].retornarNeumaticoNuevoDeParada;
 
 /*--Vistas -- (falta)
 
